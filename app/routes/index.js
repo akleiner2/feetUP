8e50c574734e2d729fed0317d65dc4928742aacb
Nevilel Jos <josneville@yahoo.com>
2014-06-28 13:46:52 -0500

    node skeleton

---
+++ app/routes/index.js
@@ -0,0 +1,5 @@
+module.exports = function(app){
+    app.get('/', function(req, res){
+		res.send(200, 'Hello World');
+	});
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+var express 	= require('express');
+var app 		= express();
+var mongoose 	= require('mongoose');
+var morgan 		= require('morgan');
+var bodyParser 	= require('body-parser');
+
+var database = require('./config/database');
+mongoose.connect(database.url);
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env){
+	app.use(morgan('dev'));
+}
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser());
+
+require('./app/routes/index.js')(app);
+
+app.listen(3000);
+console.log("App is running on port 3000");
